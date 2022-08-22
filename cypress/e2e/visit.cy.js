efe99001b8b6fa2319d94ef8c7cf91d30f7cad12
Victoria de la Plaza <101564372+victoriamariadlp@users.noreply.github.com>
2022-08-21 22:02:23 -0300

    commit inicial

---
+++ cypress/e2e/visit.cy.js
@@ -0,0 +1,10 @@
+describe('navegando a un sitio', () => {
+
+    it('visita sitio admin', () => {
+        // We use the `cy.get()` command to get all elements that match the selector.
+        // Then, we use `should` to assert that there are two matched items,
+        // which are the two default items.
+        cy.visit('https://admin.woki.ar');
+        cy.get('input[type=email]').select('')
+      })
+});
