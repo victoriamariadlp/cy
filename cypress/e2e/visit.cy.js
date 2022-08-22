b98eb4f52922eb41cd414d9a3fea2c03bc05f5aa
Victoria de la Plaza <101564372+victoriamariadlp@users.noreply.github.com>
2022-08-21 22:33:58 -0300

    se agrega chequeo de titulo

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -4,6 +4,7 @@ describe('navegando a un sitio', () => {
         // We use the `cy.get()` command to get all elements that match the selector.
         // Then, we use `should` to assert that there are two matched items,
         // which are the two default items.
-        cy.visit('https://admin.woki.ar');yrj
+        cy.visit('https://admin.woki.ar');
+        cy.title().should('contain', 'Woki');
       })
 });
