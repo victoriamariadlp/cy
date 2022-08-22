14fc617908158eb3bbc2307173c1c09ef6a91aef
Victoria de la Plaza <101564372+victoriamariadlp@users.noreply.github.com>
2022-08-21 22:09:04 -0300

    Update visit.cy.js

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -4,6 +4,6 @@ describe('navegando a un sitio', () => {
         // We use the `cy.get()` command to get all elements that match the selector.
         // Then, we use `should` to assert that there are two matched items,
         // which are the two default items.
-        cy.visit('https://admin.woki.ar');
+        cy.visit('https://admin.woki.ar');yrj
       })
 });
